4873c3f4e94a66acd1c843caa1011bcca4f79fe4
SivadharshiniMohan <sivadharshini25001@gmail.com>
2025-05-08 20:49:25 +0530

    deploying

--- src/components/common/bookNow.js
+++ src/components/common/bookNow.js
@@ -1,8 +1,7 @@
 
 export const initiateUpiPayment = (amount) => {
     const upiUrl = `upi://pay?pa="8056750121@pthdfc"&pn="Sivadharshini M"&am=${amount}&cu=INR`;
-
-  window.location.href = upiUrl;
+   window.location.href = upiUrl;
 };
 
 
