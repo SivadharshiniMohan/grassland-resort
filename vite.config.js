8adce7ee0810434a76149de42167e30804595163
SivadharshiniMohan <sivadharshini25001@gmail.com>
2025-12-18 23:13:37 +0530

    Fix: Rename .js files to .jsx for Vite JSX support and update index.html script path

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+  plugins: [react({
+    include: /src\/.*\.[jt]sx?$/,
+  })],
   server: {
     port: 3001,
   },
   publicDir: 'public',
+  build: {
+    minify: 'esbuild',
+  }
 })
